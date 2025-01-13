bf0067074651353df82fca1053dfcbc209acc1aa
Atharva Agnihotri <141016822+AtharvaAgnihotri05@users.noreply.github.com>
2025-01-13 05:18:12 +0000

    typecasting

---
+++ basics/conversion.js
@@ -0,0 +1,13 @@
+// let age = true
+// //console.log(typeof score);
+
+// let valueNumber = Number(age)
+// console.log(typeof (age));
+// console.log(typeof(valueNumber));
+// console.log(valueNumber);
+
+
+
+let isboolen = ""
+let converttobool = Boolean(isboolen)
+console.log(converttobool); 
