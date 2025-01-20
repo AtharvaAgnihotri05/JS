229638a2bcd8870d0794833afad9606cf936830d
Atharva Agnihotri <141016822+AtharvaAgnihotri05@users.noreply.github.com>
2025-01-20 06:02:15 +0000

    basic date and time

---
+++ basics/date_and_time.js
@@ -0,0 +1,15 @@
+// let mydate = new Date()
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toJSON())
+
+// console.log(typeof(mydate))
+
+// create you own date
+// let createdate = new Date(2025, 4, 23,5,3)
+// console.log(createdate.toLocaleString())
+
+// timestamps
+let mytimestamp = Date.now()
+console.log(mytimestamp/1000)
+console.log(Math.floor(mytimestamp/1000))
