6441d4eaa3b6eabbc760795f3c84f8b35a2d3e8a
Atharva Agnihotri <141016822+AtharvaAgnihotri05@users.noreply.github.com>
2025-01-20 05:44:59 +0000

    basic maths

--- basics/math.js
+++ basics/math.js
@@ -1,3 +1,19 @@
-let mynumber = "74";
-mynumber = Number(mynumber);
-console.log(mynumber + 3);
+// let mynumber = "74";
+// mynumber = Number(mynumber);
+// console.log(mynumber + 3);
+
+
+// console.log(Math.abs(-100))
+
+// console.log(Math.round(-100.678))
+
+// console.log(Math.sqrt(-100))
+
+// console.log(Math.random() * 10)
+
+// code to find value between specific range
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random()*(max - min + 1) + min))
