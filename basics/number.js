17e0a24a28b71af284a7ea56f226085d781a8244
Atharva Agnihotri <141016822+AtharvaAgnihotri05@users.noreply.github.com>
2025-01-20 05:33:56 +0000

    basic number operations

---
+++ basics/number.js
@@ -0,0 +1,12 @@
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+
+const othernumber = 123.891
+console.log(othernumber.toPrecision(3))
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-IN'))
