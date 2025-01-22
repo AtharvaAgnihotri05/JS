f88cb24a54f27be8a9912b8709afad78e5d63d52
Atharva Agnihotri <141016822+AtharvaAgnihotri05@users.noreply.github.com>
2025-01-22 08:01:37 +0000

    basic functions

---
+++ functions/function_01.js
@@ -0,0 +1,14 @@
+// function myname(){
+//     console.log("A");
+//     console.log("G");
+//     console.log("A");
+// }
+
+// myname()
+
+function addtwonumber(number1 , number2)
+{
+    console.log(number1 + number2);
+}
+
+addtwonumber(2,"A")
