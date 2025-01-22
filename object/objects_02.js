1fed333db47ffb18e7b7075686ce643844a85a3b
Atharva Agnihotri <141016822+AtharvaAgnihotri05@users.noreply.github.com>
2025-01-22 07:41:57 +0000

    json

---
+++ object/objects_02.js
@@ -0,0 +1,39 @@
+// const tinderuser = new Object();
+// tinderuser.id = "123AGA"
+// tinderuser.name = "XYZ"
+
+// console.log(tinderuser);
+
+
+// const user = {
+//     id : "123a",
+//     fullname : {
+//         firstname : "A",
+//         middlename : "B",
+//         lastname : "c"
+//     },
+//     email : "abc@gmail.com"
+// }
+
+// console.log(user.fullname.firstname);
+
+
+// merge two objects
+
+// const obj1 = {1 : "a",2 : "b"}
+// const obj2 = {3 : "c",4 : "d"}
+
+// const obj3 = Object.assign(obj1,obj2)
+// console.log(obj3);
+
+// console.log(Object.keys(obj3));
+// console.log(Object.entries(obj3));
+
+const course = {
+    coursename : "JS",
+    price : 1000
+}
+
+const {coursename} = course
+
+console.log(coursename);
